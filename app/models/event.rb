569eb12987f326769ae67dde321e45efdc4858e1
Terence30 <terence30360@gmail.com>
2019-11-04 14:26:52 +0100

    First Push

---
+++ app/models/event.rb
@@ -0,0 +1,30 @@
+class Event < ApplicationRecord
+	has_many :attendances
+	has_many :users, through: :attendances
+	belongs_to :admin, class_name: "User"
+
+    validates :start_date, presence: true
+	validate :event_past
+	validates :duration, presence: true
+	validate :multiple_of_5
+	validates :title, presence: true, length: { in: 5..140 }
+	validates :description, presence: true, length: { in: 5..140 }
+	validates_numericality_of :price, presence: true, :only_integer => true, :greater_than_or_equal_to => 1
+    validates_numericality_of :price, :only_integer => true, :less_than_or_equal_to => 1000
+    validates :location, presence: true
+
+	private
+
+	def event_past
+      if start_date < DateTime.now
+      	errors.add(:start_date, "La date de départ de l'event ne peut pas etre dans le passé")
+      end
+    end
+
+    def multiple_of_5
+      if duration % 5 != 0
+      	errors.add(:duration, "La durée doit etre un multiple de 5")
+      end
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.all.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('users') #commande pour reset l'id 
+
+10.times do
+	u = User.create(description: Faker::TvShows::GameOfThrones.quote, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    u.email = u.last_name + "@yopmail.com"
+    u.save
+end
+puts "User created"
