642b7a1bbd2d0bd48a33f48971debba9d4b39b7b
Terence30 <terence30360@gmail.com>
2019-11-04 15:00:34 +0100

    Mailler User dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-	has_many :events, through: :attendances
-	has_many :admin_event, foreign_key: 'admin_id', class_name: "Event"
+  after_create :welcome_send
+
+  has_many :events, through: :attendances
+  has_many :admin_event, foreign_key: 'admin_id', class_name: "Event"
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 
 end
