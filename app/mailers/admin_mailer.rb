9c04ec2c586db58ee7e4197af5adca0247033d96
Terence30 <terence30360@gmail.com>
2019-11-04 15:57:27 +0100

    Mailer admin

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,14 @@
+class AdminMailer < ApplicationMailer
+  default from: 'no-reply@monsite.fr'
+ 
+  def new_participant(admin)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @admin = admin.event.admin
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @admin.email, subject: 'Nouveau participant') 
+  end
+end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,11 @@
 class Attendance < ApplicationRecord
+    after_create :new_participant_send
 	belongs_to :user
 	belongs_to :event
 
+  def new_participant_send
+  	    puts self
+    AdminMailer.new_participant(self).deliver_now
+
+  end
 end
