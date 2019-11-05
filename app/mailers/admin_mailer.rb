d1182aa0c23856df1821aedfd01869ed5a826bd8
Terence30 <terence30360@gmail.com>
2019-11-05 12:50:56 +0100

    email forget

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -6,7 +6,7 @@ class AdminMailer < ApplicationMailer
     @admin = admin.event.admin
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'http://eventbrite-thpp.herokuapp.com' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @admin.email, subject: 'Nouveau participant') 
